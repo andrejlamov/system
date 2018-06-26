b18edc8e73fe901c52b994c76f778ee45a29c6ac
Andrej Lamov <andrej.lamov@gmail.com>
2018-06-27 00:12:44 +0100

    Cleanup

--- src/system/example.cljs
+++ src/system/example.cljs
@@ -18,98 +18,87 @@
     :as all} event-ch]
   [:div [(wa counter-number) (wa counter-buttons event-ch) (wa system-tick)]])
 
-(def state-merger
-  (s/reducer "state-merger" merge))
+(defn mount-hiccup [acc hiccup event-ch]
+  (let [acc1 (merge acc hiccup)]
+    (rum/mount (rumc
+                (root acc1 event-ch))
+               (. js/document getElementById "root"))
+    acc1))
 
-(def hiccup-mounter
-  (s/async-reducer "hiccup-mounter"
-                   (fn [acc hiccup event-ch]
-                     (let [acc1 (merge acc hiccup)]
-                       (rum/mount (rumc
-                                   (root acc1 event-ch))
-                                  (. js/document getElementById "root"))
-                       acc1))))
+(defn update-counter [event]
+  (match event
+         [:inc :counter _ m] {:counter (update m :number inc)}
+         [:dec :counter _ m] {:counter (update m :number dec)}
+         _ {}))
 
-(defn logger [line]
-  (s/mapper "logger" (fn [v] (println line v) v)))
+(defn buttons-hiccup [{:keys [number] :as materialized}]
+  {:counter-buttons (fn [event-ch]
+                      [:div
+                       [:button
+                        {:on-click #(as/put! event-ch [:dec :counter nil materialized])}
+                        "decrement"]
+                       [:button
+                        {:on-click #(as/put! event-ch [:inc :counter nil materialized])}
+                        "increment"]])})
 
-(def counter-materializer
-  (s/mapper "counter/materializer" :counter))
+(defn counter-hiccup [{:keys [number] :as materialized}]
+  {:counter-number (fn []
+                     [:h3 (str "counter: " number)])})
 
-(def system-materializer
-  (s/mapper "system/materializer" :system))
+(defn update-system [event]
+  (match event
+         [:tick :system _ m] m
+         _ {}))
 
-(def counter-updater
-  (s/mapper "counter/updater"
-            (fn [event]
-              (match event
-                     [:inc :counter _ m] {:counter (update m :number inc)}
-                     [:dec :counter _ m] {:counter (update m :number dec)}
-                     _ {}))))
+(defn ticker-signal-hiccup [{:keys [tick]}]
+  {:system-tick (fn [] [:h1 (if tick "high" "low")])})
 
-(def counter-buttons
-  (s/mapper "counter/hiccuper/buttons"
-            (fn [{:keys [number] :as materialized}]
-              {:counter-buttons (fn [event-ch]
-                                  [:div
-                                   [:button
-                                    {:on-click #(as/put! event-ch [:dec :counter nil materialized])}
-                                    "decrement"]
-                                   [:button
-                                    {:on-click #(as/put! event-ch [:inc :counter nil materialized])}
-                                    "increment"]])})))
-
-(def counter-number
-  (s/mapper "counter/hiccuper/number"
-            (fn [{:keys [number] :as materialized}]
-              {:counter-number (fn []
-                                 [:h3 (str "counter: " number)])})))
-
-(def tick-updater
-  (s/mapper "system/updater" (fn [event]
-                               (match event
-                                      [:tick :system _ m] m
-                                      _ {}))))
-
-(def ticker-signal
-  (s/mapper "system/hiccuper/tick-signal"
-            (fn [{:keys [tick]}]
-              {:system-tick (fn [] [:h1 (if tick "high" "low")])})))
-
-(def ticker
-  (s/source "ticker" (fn [in out]
-                       (as/go-loop [high? true]
-                         (let [[v ch] (as/alts! [(as/timeout 1000) in])]
-                           (if (not (= ch in))
-                             (do
-                               (as/>! out [:tick :system high? {:system {:tick high?}}])
-                               (recur (not high?)))
-                             (println "ticker shutdown")))))))
+(defn ticker [in out]
+  (as/go-loop [high? true]
+    (let [[v ch] (as/alts! [(as/timeout 1000) in])]
+      (if (not (= ch in))
+        (do
+          (as/>! out [:tick :system high? {:system {:tick high?}}])
+          (recur (not high?)))
+        (println "ticker shutdown")))))
 
 (defonce graph (atom {}))
 (s/close @graph)
 
-(def scheme
-  [
-   [state-merger s/mult "state.mult"]
-   ["event.in" s/mult "event.mult"] ;format: [event-name origin value materialized]
-   [hiccup-mounter "event.in"]
+(defn scheme []
+  (let [
+        state-merger (s/reducer "state-merger" merge)
+        hiccup-mounter (s/async-reducer "hiccup-mounter" mount-hiccup)
+        counter-materializer (s/mapper "counter-materializer" :counter)
+        counter-hiccup (s/mapper "counter-hiccup" counter-hiccup)
+        counter-updater (s/mapper "counter-updater" update-counter)
+        buttons-hiccup (s/mapper "buttons-hiccup" buttons-hiccup)
+        system-materializer (s/mapper "system-materializer" :system)
+        system-updater (s/mapper "system-updater" update-system)
+        ticker-signal-hiccup (s/mapper "ticker-signal-hiccup" ticker-signal-hiccup)
+        ticker (s/source "ticker" ticker)
 
-   ["event.mult" tick-updater state-merger]
-   ["event.mult" counter-updater state-merger]
+        schema [
+                [state-merger s/mult "state.mult"]
+                ["event.in" s/mult "event.mult"]
+                [hiccup-mounter "event.in"]
+                ["event.mult" system-updater state-merger]
+                ["event.mult" counter-updater state-merger]
 
-   ["state.mult" counter-materializer s/mult "counter.mult"]
-   ["counter.mult" counter-number hiccup-mounter]
-   ["counter.mult" counter-buttons hiccup-mounter]
-   ["state.mult" system-materializer ticker-signal hiccup-mounter]
-   [ticker "event.in"]])
+                ["state.mult" counter-materializer s/mult "counter.mult"]
+                ["counter.mult" counter-hiccup hiccup-mounter]
+                ["counter.mult" buttons-hiccup hiccup-mounter]
+                ["state.mult" system-materializer ticker-signal-hiccup hiccup-mounter]
+                [ticker "event.in"]]
+        ]
+    ;; initial state
+    (as/put! (:in state-merger) {:counter {:number 0 } :system {:tick nil}})
 
-(reset! graph (s/connect scheme))
+    schema))
+
+(reset! graph (s/connect (scheme)))
 
 @graph
-
-;; initial state
-(as/put! (:in state-merger) {:counter {:number 0 } :system {:tick nil}})
 
 (defn on-js-reload []
   (println "reload!"))
